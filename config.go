431d4359a5b8e69709d0dc5592fdf94e2e97c010
Tyson Tate <tyson@tysontate.com>
2015-09-28 18:07:07 -0700

    Remove Bind() and Port() from Config.

--- config.go
+++ config.go
@@ -1,5 +1,7 @@
 package tempredis
 
+import "fmt"
+
 // Config is a simple map of config keys to config values. These config values
 // will be fed to redis-server on startup.
 type Config map[string]string
@@ -7,27 +9,27 @@ type Config map[string]string
 // Address returns the dial-able address for a Redis server configured with
 // this Config struct.
 func (c Config) Host() string {
-	return c.Bind() + ":" + c.Port()
-}
-
-// Bind returns the local bind interface for a Redis server configured with
-// this Config struct.
-func (c Config) Bind() string {
 	bind, ok := c["bind"]
 	if !ok {
 		bind = "127.0.0.1"
 	}
-	return bind
-}
 
-// Port returns the local port for a Redis server configured with this Config
-// struct.
-func (c Config) Port() string {
 	port, ok := c["port"]
 	if !ok {
 		port = "6379"
 	}
-	return port
+
+	return fmt.Sprintf("%s:%s", bind, port)
+}
+
+// URL returns a Redis URL for a Redis server configured with this Config.
+func (c Config) URL() string {
+	password := c.Password()
+	if len(password) == 0 {
+		return fmt.Sprintf("redis://%s", c.Host())
+	} else {
+		return fmt.Sprintf("redis://:%s@%s", password, c.Host())
+	}
 }
 
 // Password returns the required password for a Redis server configured with
